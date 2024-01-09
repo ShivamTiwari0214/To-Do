d52888b2c8e1b4117107d44322a8b09e73e69b20
Shivam Tiwari <st472438@gmail.com>
2024-01-09 20:01:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -124,6 +124,8 @@ app.post("/delete", (req,res) => {
 })
 
 
-app.listen(3000, () => {
-    console.log("Server started on port 3000.")
-})
+const PORT = process.env.PORT ;
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
+});
